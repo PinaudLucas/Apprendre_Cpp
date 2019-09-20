21e343cb5d32a4658b19e2bb85f3c1af364b597f
PinaudLucas <lucas.pinaud72@gmail.com>
2019-09-20 11:49:03 +0200

    refonte & add heritage classe

--- 03_Heritage/01_Contenant/carton.cpp
+++ 03_Heritage/01_Contenant/carton.cpp
@@ -4,14 +4,14 @@
 using namespace std;
 
 Carton::Carton(const int _largeur, const int _hauteur, const int _profondeur, const float _poidsMaxi):
-Contenant(_largeur,_hauteur,_profondeur),
+    Contenant(_largeur,_hauteur,_profondeur),
 
-poidsMaxi(_poidsMaxi)
+    poidsMaxi(_poidsMaxi)
 {
-cout << "Constructeur de la classe Carton" << endl;
+    cout << "Constructeur de la classe Carton" << endl;
 }
 
 float Carton::ObtenirPoidsMaxi()
 {
-return poidsMaxi;
+    return poidsMaxi;
 }
